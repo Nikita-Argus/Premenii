7e89bb2bc0eddce3d67d7ed9e2f75b255bf6b054
Nikita-Argus <ggdemon228@gmail.com>
2024-07-03 22:38:09 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+NumberOfHomework = 12
+NumberOfHours = 1.5
+CourseName = "Python"
+
+name = NumberOfHomework / NumberOfHours
+
+print('Курс:', CourseName, ', всего задач:', NumberOfHomework, ', затрачено часов:', NumberOfHours,
+      'среднее время выполнения', name, 'часа.')
